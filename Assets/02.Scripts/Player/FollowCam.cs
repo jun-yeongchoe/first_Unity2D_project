7852f088be5b2b2f5b306f1379771475c36b13b5
polit8647 <polit8647@gmail.com>
2025-10-23 18:07:10 +0900

    player move, shoot, bullet fin commit

---
+++ Assets/02.Scripts/Player/FollowCam.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    [Header("¼³Á¤")]
+    [SerializeField] Transform player;
+    private void LateUpdate()
+    {
+        Vector3 targetPos = new Vector3(player.position.x, player.position.y, this.transform.position.z);
+        transform.position = targetPos;
+
+        Debug.Log(targetPos);
+    }
+}
