6c56e259ccae26a0e186b29eec6478c3777cd9b2
polit8647 <polit8647@gmail.com>
2025-10-30 15:04:02 +0900

    Ranking System Finish Commit

--- Assets/02.Scripts/UI/FinishScene.cs
+++ Assets/02.Scripts/UI/FinishScene.cs
@@ -4,9 +4,14 @@ using System.Globalization;
 using System.IO;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class FinishScene : MonoBehaviour
 {
+    const string URL = "https://script.google.com/macros/s/AKfycbxe6XUrCnTnFOJUCyYas93qZD86wrFf_-0wBXuJ9GRsQH6FP9n-zZkGuohkx6JlsxIx/exec";
+    public string csvURL = "https://docs.google.com/spreadsheets/d/1wKliWH_XftOBHAu0sPX6vb9qumEbruhmRMXujSh5GM0/export?format=csv";
+    int lastRow;
+
     [SerializeField] TextMeshProUGUI Name;
     [SerializeField] TextMeshProUGUI Score;
     void Start()
@@ -17,6 +22,7 @@ public class FinishScene : MonoBehaviour
             Score.text = ToMMSSMS(loaded.playTime);
 
             AppendCsv(loaded.playerName, loaded.playTime);
+            StartCoroutine(Register(loaded.playerName, loaded.playTime));
         }
     }
 
@@ -37,19 +43,59 @@ public class FinishScene : MonoBehaviour
 
         using (var sw = new StreamWriter(path, append: true))
         {
-            if (!exists) sw.WriteLine("시간, 이름, 점수(시간)");
+            if (!exists) sw.WriteLine("시간, 이름, 점수(시간), 걸린시간(초)");
 
             string nameEsc = EscapeCsv(pName);
 
             string formatted = ToMMSSMS(pTime);
+            
 
-            sw.WriteLine($"{System.DateTime.UtcNow:yyyy-MM-dd HH:mm:ss},\"{nameEsc}\", {formatted}\"");
+            sw.WriteLine($"{System.DateTime.UtcNow:yyyy-MM-dd HH:mm:ss},\"{nameEsc}\", \"{formatted}\", {pTime}");
         }
         Debug.Log($"CSV 저장 완료 {path}");
     }
     private string EscapeCsv(string s)
     {
-        if (string.IsNullOrEmpty(s)) return "";
-        return s.Replace("\"", "\"\"");
+        if (string.IsNullOrEmpty(s)) return ""; // Name 칸이 비어있으면(null) 무조건 쌍따옴표 두개로 변환
+        return s.Replace("\"", "\"\"");     // 쌍따옴표가 하나만 사용되어있으면 두개로 대체
+    }
+
+    IEnumerator GetLastRow()
+    {
+        using (UnityWebRequest www = UnityWebRequest.Get(URL))
+        {
+            yield return www.SendWebRequest();
+
+            var number = www.downloadHandler.text?.Trim();
+            lastRow = int.Parse(number);
+        }
+    }
+
+    IEnumerator Post(WWWForm form)
+    {
+        using (UnityWebRequest www = UnityWebRequest.Post(URL, form)) 
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isDone) print(www.downloadHandler.text);
+            else print("웹의 응답이 없습니다.");
+        }
+    }
+
+    IEnumerator Register(string pName, float pTime)
+    {
+        yield return StartCoroutine(GetLastRow());
+
+        int newRow = lastRow + 1;
+
+        WWWForm form = new WWWForm();
+        
+        form.AddField("realtime", $"{System.DateTime.UtcNow:yyyy-MM-dd HH:mm:ss}");
+        form.AddField("name", pName);
+        form.AddField("score", ToMMSSMS(pTime)+"\"");
+        form.AddField("truescore", pTime.ToString());
+        form.AddField("rank", $"=RANK(D{newRow},D:D,1)");
+
+        yield return StartCoroutine(Post(form));
     }
 }
---
+++ Assets/02.Scripts/UI/GoogleSheetManager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.Windows;
+
+public class GoogleSheetManager : MonoBehaviour
+{
+    public string csvURL = "https://docs.google.com/spreadsheets/d/1wKliWH_XftOBHAu0sPX6vb9qumEbruhmRMXujSh5GM0/export?format=csv";
+    private string[,] rank = new string[3, 2];
+    [SerializeField] private TextMeshProUGUI[] name;
+    [SerializeField] private TextMeshProUGUI[] score;
+
+
+    void Start()
+    {
+        StartCoroutine(ImportCSV());
+    }
+
+    IEnumerator ImportCSV()
+    {
+        UnityWebRequest www = UnityWebRequest.Get(csvURL);
+        yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log("Download failed");
+            yield break;
+        }
+
+        string[] lines = www.downloadHandler.text.Split('\n');
+
+        for (int i = 1; i < lines.Length; i++)
+        {
+            if (string.IsNullOrWhiteSpace(lines[i])) continue;
+
+            string[] values = lines[i].Split(',');
+
+            if (int.Parse(values[4]) == 1)
+            {
+                for (int c = 0; c < rank.GetLength(1); c++) 
+                {
+                    rank[0 , c] = values[c+1];
+                }
+                
+            }
+            else if (int.Parse(values[4]) == 2)
+            {
+                for (int c = 0; c < rank.GetLength(1); c++)
+                {
+                    rank[1, c] = values[c + 1];
+                }
+            }
+            else if (int.Parse(values[4]) == 3)
+            {
+                for (int c = 0; c < rank.GetLength(1); c++)
+                {
+                    rank[2, c] = values[c + 1];
+                }
+            }
+            else continue;
+        }
+        for (int i = 0; i < name.Length; i++) 
+        {
+            name[i].text = rank[i, 0];
+            score[i].text = rank[i, 1].ToString().Replace('"', ' ');
+        }
+
+    }
+}
